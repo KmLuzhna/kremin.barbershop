06e5b1eec236e9b4796ccf19e5e1c95ff49a1914
Kristina Luzhna <101525972+KmLuzhna@users.noreply.github.com>
2023-07-23 02:46:32 +0300

    added  barbs-slider

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,23 +1,83 @@
-// $(document).ready(function() {
-//   // Отримати всі зображення галереї
-//   var images = $('gallery-img ');
+// // $(document).ready(function() {
+// //   // Отримати всі зображення галереї
+// //   var images = $('gallery-img ');
 
-//   // Додати клас "active" до першого зображення
-//   images.first().addClass('active');
+// //   // Додати клас "active" до першого зображення
+// //   images.first().addClass('active');
 
-//   // Клік на зображенні
-//   images.click(function() {
-//     // Зняти клас "active" з усіх зображень
-//     images.removeClass('active');
-//     // Додати клас "active" до клікнутого зображення
-//     $(this).addClass('active');
-//   });
+// //   // Клік на зображенні
+// //   images.click(function() {
+// //     // Зняти клас "active" з усіх зображень
+// //     images.removeClass('active');
+// //     // Додати клас "active" до клікнутого зображення
+// //     $(this).addClass('active');
+// //   });
+// // });
+
+// const images = document.querySelector('.gallery-img ');
+
+// const addActive = () => {
+//     // images.removeClass('active');
+//     this.addClass('active');
+// };
+// images.addEventListener("click", addActive);
+
+
+// let offset = 0;
+// const sliderLineGal = document.querySelector('.slider-line__gal');
+
+// document.querySelector('.slider-next__gal').addEventListener('click', function(){
+//     offset = offset + 256;
+//     if (offset > 768) {
+//         offset = 0;
+//     }
+//     sliderLineGal.style.left = -offset + 'px';
 // });
 
-const images = document.querySelector('.gallery-img ');
+// document.querySelector('.slider-prev__gal').addEventListener('click', function () {
+//     offset = offset - 256;
+//     if (offset < 0) {
+//         offset = 768;
+//     }
+//     sliderLineGal.style.left = -offset + 'px';
+// });
 
-const addActive = () => {
-    // images.removeClass('active');
-    this.addClass('active');
-};
-images.addEventListener("click", addActive);
+// const images = document.querySelectorAll('.gallery .gallery-line img');
+// const sliderLineGal = document.querySelector('.gallery .gallery-line');
+// let countGal = 0;
+// let widthGal;
+
+// function init() {
+//     console.log('resize');
+//     widthGal = document.querySelector('.gallery').offsetWidth;
+//     sliderLineGal.style.width = widthGal * images.length + 'px';
+//     images.forEach(item => {
+//         item.style.width = widthGal + 'px';
+//         item.style.height = 'auto';
+//     });
+//     rollSlider();
+// }
+
+// init();
+// window.addEventListener('resize', init);
+
+// document.querySelector('.gallery-next').addEventListener('click', function () {
+//     countGal++;
+//     if (countGal >= images.length) {
+//         countGal = 0;
+//     }
+//     rollSlider();
+// });
+
+// document.querySelector('.gallery-prev').addEventListener('click', function () {
+//     countGal--;
+//     if (countGal < 0) {
+//         countGal = images.length - 1;
+//     }
+//     rollSlider();
+// });
+
+// function rollSlider() {
+//     sliderLineGal.style.transform = 'translate(-' + countGal * widthGal + 'px)';
+
+// }
--- src/js/pluses.js
+++ src/js/pluses.js
@@ -3,9 +3,11 @@ const plusName = document.querySelector(".plus-info__name");
 const plusInfo = document.querySelector(".plus-info__text");
 const plusImage = document.querySelector(".plus-img__conteiner");
 const professionalismBtn = document.querySelector(".professionalism-btn");
+const professionalismBtnMob = document.querySelector(".professionalism-mob-btn");
 const qualityBtn = document.querySelector(".quality-btn");
 const modernityBtn = document.querySelector(".modernity-btn");
 const bonusesBtn = document.querySelector(".bonuses-btn");
+const mobBtnList = document.querySelector(".mob-btn-list");
 
 let photo = document.createElement("img");
 photo.src = "./quality.92b2337c.png";
@@ -17,6 +19,7 @@ function changePlus(newPlus) {
         plusInfo.innerHTML = 'Відповідальні майстри від 7 років досвіду, які знаються на сучасних трендах, постійно вдосконалюються та можуть довести твій образ до ідеалу';
         plusImage.innerHTML = '<img class="plus-img" src="/professionalism.6adffebe.png" alt="professionalism" />';
         professionalismBtn.classList.add('active');
+        professionalismBtnMob.classList.add('active');
         qualityBtn.classList.remove('active');
         modernityBtn.classList.remove('active');
         bonusesBtn.classList.remove('active');
@@ -27,6 +30,7 @@ function changePlus(newPlus) {
         plusImage.innerHTML = '<img class="plus-img" src="/quality.92b2337c.png" alt="quality" />';
         qualityBtn.classList.add('active');
         professionalismBtn.classList.remove('active');
+        professionalismBtnMob.classList.remove('active');
         modernityBtn.classList.remove('active');
         bonusesBtn.classList.remove('active');
     } else if (newPlus === "modernity") {
@@ -35,6 +39,7 @@ function changePlus(newPlus) {
         plusImage.innerHTML = '<img class="plus-img" src="/modernity.44d8ca5a.png" alt="modernity" />';
         modernityBtn.classList.add('active');
         professionalismBtn.classList.remove('active');
+        professionalismBtnMob.classList.remove('active');
         bonusesBtn.classList.remove('active');
         qualityBtn.classList.remove('active');
     } else if (newPlus === "bonuses") {
@@ -43,8 +48,15 @@ function changePlus(newPlus) {
         plusImage.innerHTML = '<img class="plus-img" src="/bonuses.6b6da01e.png" alt="bonuses" />';
         bonusesBtn.classList.add('active');
         professionalismBtn.classList.remove('active');
+        professionalismBtnMob.classList.remove('active');
         modernityBtn.classList.remove('active');
         qualityBtn.classList.remove('active');
     }      
 }
  
+// const clone = mobBtnList.clone();
+// clone.insertAdjacentHTML("afterend", '.plus-char');
+
+// mobBtnList.clone().insertBefore('.plus-char');
+// var clonedElement = mobBtnList.cloneNode(true);
+// document.body.appendChild(clonedElement);
