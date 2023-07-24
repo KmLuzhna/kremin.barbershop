fc5fdb0f8ef371406608921fffae3e3c5408f4ca
Kristina Luzhna <101525972+KmLuzhna@users.noreply.github.com>
2023-07-24 22:34:24 +0300

    added picture

--- src/js/pluses.js
+++ src/js/pluses.js
@@ -17,7 +17,7 @@ function changePlus(newPlus) {
     if (newPlus === "professionalism") {
         plusName.innerHTML = 'Професійність';
         plusInfo.innerHTML = 'Відповідальні майстри від 7 років досвіду, які знаються на сучасних трендах, постійно вдосконалюються та можуть довести твій образ до ідеалу';
-        plusImage.innerHTML = '<img class="plus-img" src="/professionalism.6adffebe.png" alt="professionalism" />';
+        plusImage.innerHTML = '<img class="plus-img" src="https://KmLuzhna.github.io/kremin.barbershop/professionalism.6adffebe.png" alt="professionalism" />';
         professionalismBtn.classList.add('active');
         professionalismBtnMob.classList.add('active');
         qualityBtn.classList.remove('active');
@@ -27,7 +27,7 @@ function changePlus(newPlus) {
         plusName.innerHTML = 'Якість';
         plusInfo.innerHTML = 'Інвестували в найкраще обладнання та кращі продукти для догляду за волоссям та шкірою. Працюємо за принципом- якість в кожній деталі'; 
         // plusImage.append(photo);
-        plusImage.innerHTML = '<img class="plus-img" src="/quality.92b2337c.png" alt="quality" />';
+        plusImage.innerHTML = '<img class="plus-img" src="https://KmLuzhna.github.io/kremin.barbershop/quality.92b2337c.png" alt="quality" />';
         qualityBtn.classList.add('active');
         professionalismBtn.classList.remove('active');
         professionalismBtnMob.classList.remove('active');
@@ -36,7 +36,7 @@ function changePlus(newPlus) {
     } else if (newPlus === "modernity") {
         plusName.innerHTML = 'Сучасність';
         plusInfo.innerHTML = 'Використовуємо тільки сучасні інструменти та чоловічу косметику, а в нашому Барбершопі крута чоловіча атмосфера з сучасним інтер’єром'; 
-        plusImage.innerHTML = '<img class="plus-img" src="/modernity.44d8ca5a.png" alt="modernity" />';
+        plusImage.innerHTML = '<img class="plus-img" src="https://KmLuzhna.github.io/kremin.barbershop/modernity.44d8ca5a.png" alt="modernity" />';
         modernityBtn.classList.add('active');
         professionalismBtn.classList.remove('active');
         professionalismBtnMob.classList.remove('active');
@@ -45,7 +45,7 @@ function changePlus(newPlus) {
     } else if (newPlus === "bonuses") {
         plusName.innerHTML = 'Бонуси';
         plusInfo.innerHTML = 'Безкоштовні напої клієнтам, та програми лояльності'; 
-        plusImage.innerHTML = '<img class="plus-img" src="/bonuses.6b6da01e.png" alt="bonuses" />';
+        plusImage.innerHTML = '<img class="plus-img" src="https://KmLuzhna.github.io/kremin.barbershop/bonuses.6b6da01e.png" alt="bonuses" />';
         bonusesBtn.classList.add('active');
         professionalismBtn.classList.remove('active');
         professionalismBtnMob.classList.remove('active');
