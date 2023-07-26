3b81bbd95a2fc232f0af73936c631fe57acd60dd
Kristina Luzhna <101525972+KmLuzhna@users.noreply.github.com>
2023-07-27 00:31:27 +0300

    finished desktop version

--- src/js/pluses.js
+++ src/js/pluses.js
@@ -9,51 +9,145 @@ const modernityBtn = document.querySelector(".modernity-btn");
 const bonusesBtn = document.querySelector(".bonuses-btn");
 const mobBtnList = document.querySelector(".mob-btn-list");
 
+const professionalismImg = document.querySelector(".professionalism-img");
+const qualityImg = document.querySelector(".quality-img");
+const modernityImg = document.querySelector(".modernity-img");
+const bonusesImg = document.querySelector(".bonuses-img");
+
+
 let photo = document.createElement("img");
 photo.src = "./quality.92b2337c.png";
 photo.alt = "quality";
 console.log(photo)
 function changePlus(newPlus) {
     if (newPlus === "professionalism") {
+        activeProfessionalis();
+    } else if (newPlus === "quality") {
+        activeQuality();
+    } else if (newPlus === "modernity") {
+        activeModernity();
+    } else if (newPlus === "bonuses") {
+        activeBonuses();
+    }      
+}
+ 
+
+function activeProfessionalis() {
         plusName.innerHTML = 'Професійність';
         plusInfo.innerHTML = 'Відповідальні майстри від 7 років досвіду, які знаються на сучасних трендах, постійно вдосконалюються та можуть довести твій образ до ідеалу';
-        plusImage.innerHTML = '<img class="plus-img" src="/professionalism.6adffebe.png" alt="professionalism" />';
-        professionalismBtn.classList.add('active');
+
+    professionalismImg.classList.remove('not-active');
+    qualityImg.classList.add('not-active');
+    modernityImg.classList.add('not-active');
+    bonusesImg.classList.add('not-active');
+
+    professionalismBtn.classList.add('active');
         professionalismBtnMob.classList.add('active');
         qualityBtn.classList.remove('active');
         modernityBtn.classList.remove('active');
         bonusesBtn.classList.remove('active');
-    } else if (newPlus === "quality") {
+        
+}
+
+function activeQuality() {
         plusName.innerHTML = 'Якість';
-        plusInfo.innerHTML = 'Інвестували в найкраще обладнання та кращі продукти для догляду за волоссям та шкірою. Працюємо за принципом- якість в кожній деталі'; 
-        // plusImage.append(photo);
-        plusImage.innerHTML = '<img class="plus-img" src="/quality.92b2337c.png" alt="quality" />';
+        plusInfo.innerHTML = 'Інвестували в найкраще обладнання та кращі продукти для догляду за волоссям та шкірою. Працюємо за принципом- якість в кожній деталі';
+    
+    qualityImg.classList.remove('not-active');
+    professionalismImg.classList.add('not-active');
+    modernityImg.classList.add('not-active');
+    bonusesImg.classList.add('not-active');
+    
         qualityBtn.classList.add('active');
         professionalismBtn.classList.remove('active');
         professionalismBtnMob.classList.remove('active');
         modernityBtn.classList.remove('active');
         bonusesBtn.classList.remove('active');
-    } else if (newPlus === "modernity") {
-        plusName.innerHTML = 'Сучасність';
-        plusInfo.innerHTML = 'Використовуємо тільки сучасні інструменти та чоловічу косметику, а в нашому Барбершопі крута чоловіча атмосфера з сучасним інтер’єром'; 
-        plusImage.innerHTML = '<img class="plus-img" src="./images/pluses/modernity.png" alt="modernity" />';
-        modernityBtn.classList.add('active');
-        professionalismBtn.classList.remove('active');
-        professionalismBtnMob.classList.remove('active');
-        bonusesBtn.classList.remove('active');
-        qualityBtn.classList.remove('active');
-    } else if (newPlus === "bonuses") {
+        
+}
+
+function activeModernity() {  
+    plusName.innerHTML = 'Сучасність';
+    plusInfo.innerHTML = 'Використовуємо тільки сучасні інструменти та чоловічу косметику, а в нашому Барбершопі крута чоловіча атмосфера з сучасним інтер’єром'; 
+        
+    modernityImg.classList.remove('not-active');
+    professionalismImg.classList.add('not-active');
+    qualityImg.classList.add('not-active');
+    bonusesImg.classList.add('not-active');  
+
+    modernityBtn.classList.add('active');
+    professionalismBtn.classList.remove('active');
+    professionalismBtnMob.classList.remove('active');
+    bonusesBtn.classList.remove('active');
+    qualityBtn.classList.remove('active');
+        
+}
+
+function activeBonuses() {
         plusName.innerHTML = 'Бонуси';
         plusInfo.innerHTML = 'Безкоштовні напої клієнтам, та програми лояльності'; 
-        plusImage.innerHTML = '<img class="plus-img" src="/bonuses.6b6da01e.png" alt="bonuses" />';
-        bonusesBtn.classList.add('active');
+     
+    bonusesImg.classList.remove('not-active');
+    professionalismImg.classList.add('not-active');
+    modernityImg.classList.add('not-active');
+    qualityImg.classList.add('not-active');
+    
+    bonusesBtn.classList.add('active');
         professionalismBtn.classList.remove('active');
         professionalismBtnMob.classList.remove('active');
         modernityBtn.classList.remove('active');
         qualityBtn.classList.remove('active');
-    }      
 }
+
+// WORK
+// function changePlus(newPlus) {
+//     if (newPlus === "professionalism") {
+//         plusName.innerHTML = 'Професійність';
+//         plusInfo.innerHTML = 'Відповідальні майстри від 7 років досвіду, які знаються на сучасних трендах, постійно вдосконалюються та можуть довести твій образ до ідеалу';
+//         plusImage.innerHTML = '<img class="plus-img" src="/professionalism.6adffebe.png" alt="professionalism" />';
+//         professionalismBtn.classList.add('active');
+//         professionalismBtnMob.classList.add('active');
+//         qualityBtn.classList.remove('active');
+//         modernityBtn.classList.remove('active');
+//         bonusesBtn.classList.remove('active');
+//     } else if (newPlus === "quality") {
+//         plusName.innerHTML = 'Якість';
+//         plusInfo.innerHTML = 'Інвестували в найкраще обладнання та кращі продукти для догляду за волоссям та шкірою. Працюємо за принципом- якість в кожній деталі'; 
+//         // plusImage.append(photo);
+//         plusImage.innerHTML = '<img class="plus-img" src="/quality.92b2337c.png" alt="quality" />';
+//         qualityBtn.classList.add('active');
+//         professionalismBtn.classList.remove('active');
+//         professionalismBtnMob.classList.remove('active');
+//         modernityBtn.classList.remove('active');
+//         bonusesBtn.classList.remove('active');
+//     } else if (newPlus === "modernity") {
+//         plusName.innerHTML = 'Сучасність';
+//         plusInfo.innerHTML = 'Використовуємо тільки сучасні інструменти та чоловічу косметику, а в нашому Барбершопі крута чоловіча атмосфера з сучасним інтер’єром'; 
+//         plusImage.innerHTML = '<img class="plus-img" src="/modernity.44d8ca5a.png" alt="modernity" />';
+//         modernityBtn.classList.add('active');
+//         professionalismBtn.classList.remove('active');
+//         professionalismBtnMob.classList.remove('active');
+//         bonusesBtn.classList.remove('active');
+//         qualityBtn.classList.remove('active');
+//     } else if (newPlus === "bonuses") {
+//         plusName.innerHTML = 'Бонуси';
+//         plusInfo.innerHTML = 'Безкоштовні напої клієнтам, та програми лояльності'; 
+//         plusImage.innerHTML = '<img class="plus-img" src="/bonuses.6b6da01e.png" alt="bonuses" />';
+//         bonusesBtn.classList.add('active');
+//         professionalismBtn.classList.remove('active');
+//         professionalismBtnMob.classList.remove('active');
+//         modernityBtn.classList.remove('active');
+//         qualityBtn.classList.remove('active');
+//     }      
+// }
  
+
+
+
+
+
+
+
 // const clone = mobBtnList.clone();
 // clone.insertAdjacentHTML("afterend", '.plus-char');
 
