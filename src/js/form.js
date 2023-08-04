1e61809c62cb5039e5bb3945380862c120171447
Kristina Luzhna <101525972+KmLuzhna@users.noreply.github.com>
2023-08-05 01:38:22 +0300

    2

--- src/js/form.js
+++ src/js/form.js
@@ -52,24 +52,53 @@ onForm.addEventListener('input', changedOnForm);
 onForm.addEventListener("submit", submitForm);
 
 
-document.getElementById("myForm").addEventListener("submit", function(event) {
-  event.preventDefault(); // Забороняємо формі відправлятись автоматично
 
-  var formData = new FormData(this); // Отримуємо дані з форми
-  var chatId = "5187801485"; // Замініть на ваш chatId, отриманий від Телеграму
 
-  // Відправляємо POST-запит на API Телеграму
-  fetch(`https://api.telegram.org/bot6444843684:AAHgazEpVJzBoNaJE6XRUbWy78z-U5jn3yE/sendMessage?chat_id=5187801485&text=Всім привіт!`, {
-    method: "POST",
-    body: formData
-  })
+// -----------------------------------
+// document.getElementById("myForm").addEventListener("submit", function(event) {
+//   event.preventDefault(); // Забороняємо формі відправлятись автоматично
+
+//   var formData = new FormData(this); // Отримуємо дані з форми
+//   var chatId = "5187801485"; // Замініть на ваш chatId, отриманий від Телеграму
+
+//   // Відправляємо POST-запит на API Телеграму
+//   fetch(`https://api.telegram.org/bot6444843684:AAHgazEpVJzBoNaJE6XRUbWy78z-U5jn3yE/sendMessage?chat_id=5187801485&text=Всім привіт!`, {
+//     method: "POST",
+//     body: formData
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data); // Результат відправки повідомлення в Телеграм
+//     // Додайте код для обробки успішної відправки повідомлення
+//   })
+//   .catch(error => {
+//     console.error(error); // Помилка відправки повідомлення в Телеграм
+//     // Додайте код для обробки помилки відправки повідомлення
+//   });
+// });
+
+const token = '6444843684:AAHgazEpVJzBoNaJE6XRUbWy78z-U5jn3yE';
+let inputConfig = input.config();
+let message = `Hello`;
+console.log(`${message}`);
+const options = {
+  method: 'POST',
+  Host: 'api.telegram.org:443',
+  headers: {
+    Accept: 'application/json',
+    'User-Agent': 'Airtable',
+    'Content-Type': 'application/json',    
+  },
+  body: JSON.stringify({
+    text: `${message}`,
+    parse_mode: 'HTML',
+    disable_web_page_preview: true,
+    disable_notification: true,
+    reply_to_message_id: 0,
+    chat_id: `${inputConfig.chat_id}`
+ })
+};
+fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${inputConfig.chat_id}`, options)
   .then(response => response.json())
-  .then(data => {
-    console.log(data); // Результат відправки повідомлення в Телеграм
-    // Додайте код для обробки успішної відправки повідомлення
-  })
-  .catch(error => {
-    console.error(error); // Помилка відправки повідомлення в Телеграм
-    // Додайте код для обробки помилки відправки повідомлення
-  });
-});
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
